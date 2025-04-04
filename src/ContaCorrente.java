29d9cb0f2ee4b36360eaab485553eb10f70db44a
Hugo <hugoribeirorneto@gmail.com>
2025-04-03 21:02:06 -0300

    início do projeto com a configuração das classes

---
+++ src/ContaCorrente.java
@@ -0,0 +1,12 @@
+public class ContaCorrente extends Conta {
+
+    @Override
+    public void sacar(double valor) {
+    }
+    @Override
+    public void depositar(double valor) {
+    }
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+    }
+}
